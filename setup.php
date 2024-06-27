c6a3cc512d33b751c20852be577a4b2e017ec887
Ieva Šinke <ieva.sinke@gmail.com>
2024-06-27 18:37:56 +0300

    datatype fix

--- setup.php
+++ setup.php
@@ -2,7 +2,6 @@
 
 require 'vendor/autoload.php';
 
-use App\Models\User;
 use Medoo\Medoo;
 
 $database = new Medoo([
@@ -14,14 +13,14 @@ $database->query("CREATE TABLE IF NOT EXISTS users (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 username VARCHAR(32) NOT NULL,
 password VARCHAR(32) NOT NULL,
-balance REAL NOT NULL
+balance FLOAT NOT NULL
 )");
 
 $database->query("CREATE TABLE IF NOT EXISTS wallets (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-symbol TEXT NOT NULL,
-amount REAL NOT NULL,
-average_price REAL NOT NULL,
+symbol VARCHAR(255) NOT NULL,
+amount FLOAT NOT NULL,
+average_price FLOAT NOT NULL,
 user_id INTEGER NOT NULL,
 FOREIGN KEY (user_id) REFERENCES users(id)
 )");
@@ -29,15 +28,12 @@ FOREIGN KEY (user_id) REFERENCES users(id)
 $database->query("CREATE TABLE IF NOT EXISTS transactions (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 user_id INTEGER NOT NULL,
-kind TEXT NOT NULL,
-symbol TEXT NOT NULL,
-price REAL NOT NULL,
-quantity REAL NOT NULL,
-created_at TEXT NOT NULL,
+kind VARCHAR(255) NOT NULL,
+symbol VARCHAR(255) NOT NULL,
+price FLOAT NOT NULL,
+quantity FLOAT NOT NULL,
+created_at DATETIME NOT NULL,
 FOREIGN KEY (user_id) REFERENCES users(id)
 )");
-
-(new User("admin", md5('admin'), 1000))->add();
-(new User("Customer", md5('654321'), 1000))->add();
 
 echo "Database schema initialized.\n";
